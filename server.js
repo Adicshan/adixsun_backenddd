0edd196f71c51f28c45b48c27dbc68dba3e787e7
Adixshan <rk8816616@gmail.com>
2023-08-27 20:20:21 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,43 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PythonShell } = require('python-shell'); // Import PythonShell
+const {spawn}= require('child_process');
 const port = process.env.PORT || 3002;
-const url = process.env.URL;
-const path = require('path');
+const url= process.env.URL;
+const path =require('path');
 const app = express();
 app.use(bodyParser.json());
-const cors = require('cors');
+const cors=require('cors');
+
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
+
+
 
 app.post('/exam', (req, res) => {
   const { toughness, hour, consist, syllabus, time } = req.body;
+  
 
-  console.log('Toughness:', toughness);
-  const pythonScriptPath = path.join(__dirname, 'scripts', 'logisticRegression.py');
+console.log('Toughness:', toughness);
+const pythonScriptPath = path.join(__dirname, "..","server",'scripts', 'logisticRegression.py');
 
-  const options = {
-    mode: 'text',
-    pythonPath: 'python', // or 'python3' depending on your system
-    pythonOptions: ['-u'], // unbuffered output
-    scriptPath: path.dirname(pythonScriptPath),
-    args: [toughness, hour, consist, syllabus, time],
-  };
+  
+  const ExamProcess = spawn('venv/bin/python', [pythonScriptPath, toughness, hour, consist, syllabus, time]);
+  
 
-  PythonShell.run(path.basename(pythonScriptPath), options, (err, results) => {
-    if (err) {
-      console.log('Python error:', err);
-      res.status(500).send({ error: err });
-    } else {
-      console.log('Python output:', results);
-      res.status(200).json({ output: results });
-    }
-  });
+ExamProcess.stdout.on('data',(data)=>{
+const output= data.toString();
+console.log('Python output:',output);
+res.status(200).json({output:output});
 });
+ExamProcess.stderr.on('data',(data)=>{
+  const error=data.toString();
+  console.log('Python error:',error);
+});
+ExamProcess.on('close',(code)=>{
+  console.log(`python process execute with code ${code}`);
+});
+
+
+});
+
 
 app.listen(port, () => {
   console.log(`Server is ng on port ${port}`);
+  
   console.log(`${url}`);
 });
