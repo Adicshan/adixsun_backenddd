83263ec03ed25881efaf0637e085481b8eaf4cdb
Adicshan <129241416+Adicshan@users.noreply.github.com>
2023-08-29 12:22:23 +0530

    Update Exam_prediction.py

--- scripts/Exam_prediction.py
+++ scripts/Exam_prediction.py
@@ -5,6 +5,10 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+pandas_path = '/opt/render/.local/lib/python3.7/site-packages'
+sklearn_path = '/opt/render/.local/lib/python3.7/site-packages'
+sys.path.append(pandas_path)
+sys.path.append(sklearn_path)
 
 file_path = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(file_path, 'Exam_data.csv')
